c2d486eacf8046280bbc7e596c8ba87ccabe53bb
danieleangeletti <angelettidaniele@gmail.com>
2024-01-24 17:54:53 +0000

    milestone 1 completata

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,22 @@
 <script>
+import { store } from "../store.js";
+import SingleFilm from "./SingleFilm.vue";
+
 export default {
   data() {
-    return {};
+    return { store };
   },
+  components: { SingleFilm },
   methods: {},
 };
 </script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="container">
+      <SingleFilm v-for="(elem, i) in store.films" :key="i" :film="elem" />
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/SingleFilm.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from "../store.js";
+
+export default {
+  data() {
+    return { store };
+  },
+  methods: {},
+  props: {
+    film: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <span>Il film è: {{ film.title }}. </span>
+      <span>Il titolo del film originale è: {{ film.original_title }}. </span>
+      <span>La lingua originale è: {{ film.original_language }}. </span>
+      <span>Il voto medio è: {{ film.vote_average }}.</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
