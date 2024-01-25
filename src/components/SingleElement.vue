6e71bd87385610dc13fc376b1e877c3e8a45b326
danieleangeletti <angelettidaniele@gmail.com>
2024-01-25 11:39:32 +0000

    milestone 2 completata

--- src/components/SingleElement.vue
+++ src/components/SingleElement.vue
@@ -5,7 +5,22 @@ export default {
   data() {
     return { store };
   },
-  methods: {},
+  methods: {
+    get_image(lang) {
+      let final_link = "https://flagicons.lipis.dev/flags/4x3/";
+
+      if (lang == "en") {
+        final_link += "gb";
+      } else if (lang == "ja") {
+        final_link += "jp";
+      } else {
+        final_link += lang;
+      }
+      final_link += ".svg";
+
+      return final_link;
+    },
+  },
   props: {
     title: String,
     original_title: String,
@@ -18,13 +33,22 @@ export default {
 <template>
   <div class="container">
     <ul>
-      <li>Il titolo è: {{ title }}.</li>
-      <li>Il titolo originale è:{{ original_title }}.</li>
-      <li>La lingua originale è: {{ original_language }}.</li>
-      <li>Il voto medio è: {{ vote }}.</li>
+      <li>
+        <div>Il titolo è: {{ title }}.</div>
+        <div>Il titolo originale è: {{ original_title }}.</div>
+        <div>La lingua originale è: {{ original_language }}.</div>
+        <div>
+          La bandiera è: <img :src="get_image(original_language)" alt="" />
+        </div>
+        <div>Il voto medio è: {{ vote }}.</div>
+      </li>
     </ul>
     <hr />
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+  height: 24px;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -7,33 +7,4 @@ export const store = reactive({
   films: [],
   series: [],
   search: "",
-  languages: [
-    "ru", // russian &#127479&#127482
-    "lt", // lithuanian &#127473&#127481
-    "et", // estonian &#127466&#127466
-    "hu", // hungarian &#127469&#127482
-    "lb", // luxembourgish &#127473&#127482
-    "at", // austrian &#127462&#127481
-    "nl", // netherlands &#127475&#127473
-    "es", // spanish &#127466&#127480
-    "pl", // polish &#127477&#127473
-    "uk", // ukrainian
-    "fr", // french &#127467&#127479
-    "be", // belgium
-    "it", // italian
-    "ga", // irish
-    "da", // danish
-    "gb", // united kingdom
-    "sv", // swedish
-    "cs", // czech
-    "el", // greek
-    "us", // usa
-    "au", // australian
-    "ca", // canadian
-    "lv", // latvian
-    "de", // deutsch
-    "ja", // japanese
-    "zh", // chinese
-    "il", // israel
-  ],
 });
